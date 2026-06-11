ffc5dbf47ec1480a66e2717c047833e6fd44d392
ishandas994-cloud <ishandas994@gmail.com>
2026-06-11 10:13:54 +0530

    app.jsx

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import Navbar from './components/layout/Navbar'
-import Sidebar from './components/layout/Sidebar'
-import Home from './pages/Home'
-import Dashboard from './pages/Dashboard'
-import Roadmap from './pages/Roadmap'
-import Interview from './pages/Interview'
-import Profile from './pages/Profile'
- 
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchCurrentUser, selectIsAuthenticated } from './store/userslice'
+import Navbar    from './components/layout/navbar'
+import Sidebar   from './components/layout/sidebar'
+import Home      from './pages/home'
+import Dashboard from './pages/dashboard'
+import Roadmap   from './pages/roadmap'
+import Interview from './pages/interview'
+import Profile   from './pages/profile'
+
 function Layout({ children }) {
   return (
     <>
@@ -21,17 +23,40 @@ function Layout({ children }) {
     </>
   )
 }
- 
+
+function ProtectedRoute({ children }) {
+  const isAuthenticated = useSelector(selectIsAuthenticated)
+  return isAuthenticated ? children : <Navigate to="/" replace />
+}
+
 export default function App() {
+  const dispatch        = useDispatch()
+  const isAuthenticated = useSelector(selectIsAuthenticated)
+
+  // Re-fetch fresh user from server whenever the app loads
+  useEffect(() => {
+    if (isAuthenticated) {
+      dispatch(fetchCurrentUser())
+    }
+  }, [])
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/dashboard" element={<Layout><Dashboard /></Layout>} />
-        <Route path="/roadmap"   element={<Layout><Roadmap /></Layout>} />
-        <Route path="/interview" element={<Layout><Interview /></Layout>} />
-        <Route path="/profile"   element={<Layout><Profile /></Layout>} />
-        <Route path="*"          element={<Navigate to="/" replace />} />
+        <Route path="/dashboard" element={
+          <ProtectedRoute><Layout><Dashboard /></Layout></ProtectedRoute>
+        } />
+        <Route path="/roadmap" element={
+          <ProtectedRoute><Layout><Roadmap /></Layout></ProtectedRoute>
+        } />
+        <Route path="/interview" element={
+          <ProtectedRoute><Layout><Interview /></Layout></ProtectedRoute>
+        } />
+        <Route path="/profile" element={
+          <ProtectedRoute><Layout><Profile /></Layout></ProtectedRoute>
+        } />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
   )
